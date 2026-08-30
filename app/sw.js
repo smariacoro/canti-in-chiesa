b8a56af31f3ab8918952dcd4325af06f6be4e03f
Claudio Schiavella <schiavella.1884561@studenti.uniroma1.it>
2026-08-30 23:03:06 +0200

    Guida in due parti, scalette condivise da tutti

--- app/sw.js
+++ app/sw.js
@@ -2,7 +2,7 @@
 // Strategia: cache-first con aggiornamento in background (stale-while-revalidate),
 // così in chiesa l'apertura è istantanea anche con una linea pessima.
 
-const VERSION = 'v5';
+const VERSION = 'v6';
 const CACHE = `canti-in-chiesa-${VERSION}`;
 
 const PRECACHE = [
